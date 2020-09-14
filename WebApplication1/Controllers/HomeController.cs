3a6d2a688ea39bfbc370453ad659f825849ce470
kun <makunhappy@gmail.com>
2020-09-15 00:30:00 +0800

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Dapper;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private IConfiguration configuration;
+        private string connectionString;
+        private string redisConf;
+        private string studentKey = "student:{0}";
+        public HomeController(IConfiguration conf)
+        {
+            this.configuration = conf;
+            this.connectionString = this.configuration.GetSection("connectionString").Value;
+            this.redisConf = this.configuration.GetSection("redisConf").Value;
+        }
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            if (id == 0 || id < 0)
+            {
+                return Json("error!");
+            }
+            using (var ex = ConnectionMultiplexer.Connect(this.redisConf))
+            {
+                var db = ex.GetDatabase();
+                if (db.KeyExists(string.Format(this.studentKey, id)))
+                {
+                    var value = db.StringGet(string.Format(this.studentKey, id));
+                    var data = JsonConvert.DeserializeObject<Student[]>(value.ToString());
+                    return Json(data);
+                }
+            }
+            using (var conn = new MySqlConnection(this.connectionString))
+            {
+                var students = conn.Query<Student, School, Student>("select * from student as stu join school sch on stu.schoolid = sch.id ", (student, school) =>
+                {
+                    student.SchoolInfo = school;
+                    return student;
+                });
+                using (var ex = ConnectionMultiplexer.Connect(this.redisConf))
+                {
+                    var db = ex.GetDatabase();
+                    db.StringSet(string.Format(this.studentKey, id), JsonConvert.SerializeObject(students));
+                }
+                return Json(students);
+            }
+            return Json(new { name = "kun", age = 12 });
+        }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public School SchoolInfo { get; set; }
+    }
+    public class School
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ demodb.sql
@@ -0,0 +1,52 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80020
+ Source Host           : localhost:3306
+ Source Schema         : demodb
+
+ Target Server Type    : MySQL
+ Target Server Version : 80020
+ File Encoding         : 65001
+
+ Date: 15/09/2020 00:14:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for school
+-- ----------------------------
+DROP TABLE IF EXISTS `school`;
+CREATE TABLE `school`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of school
+-- ----------------------------
+INSERT INTO `school` VALUES (1, 'changping');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `schoolid` int(0) NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `schoolid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, 'kun', 1, 'beijing');
+
+SET FOREIGN_KEY_CHECKS = 1;
